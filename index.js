c5b9925b8673806732ccc0b4044c57d7524063e3
AvenDing <139747334+Aven2002@users.noreply.github.com>
2024-07-25 01:39:11 +0800

    Added Brand_Logo

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ const favoriteRoutes = require('./routes/favoriteRoutes');
 const app = express();
 const port = 3000;
 
+// Enable CORS
 app.use(cors());
+
+// Parse JSON bodies
 app.use(express.json());
+
+// Serve static files from 'assets' directory
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+// API routes
 app.use('/api/equipment', equipmentRoutes);
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/favorite', favoriteRoutes);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
