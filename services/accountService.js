0a96d66f851810cfc0e5087f4c3a942faa7ae747
Aven2002 <avending2002@outlook.com>
2024-09-09 04:12:31 +0800

    Ensure `created_at` is excluded

--- services/accountService.js
+++ services/accountService.js
@@ -35,9 +35,12 @@ exports.createAccount = (newUser, callback) => {
 
 
 exports.updateAccount = (id, updatedItem, callback) => {
+  delete updatedItem.created_at; 
+
   const sql = 'UPDATE user_account SET ? WHERE userID = ?';
   db.query(sql, [updatedItem, id], callback);
 };
+
 
 exports.deleteAccount = (id, callback) => {
   const sql = 'DELETE FROM user_account WHERE userID = ?';
